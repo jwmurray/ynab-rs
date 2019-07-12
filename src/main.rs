b4050508b55b3a19b534417903ee3162da39fc25
jwmurray <john@gardenway.org>
2019-07-11 22:15:47 -0600

    Download txns

--- src/main.rs
+++ src/main.rs
@@ -19,14 +19,19 @@ fn main() {
 
     let mut easy = Easy2::new(Collector(Vec::new()));
 
-    let url = "https://api.youneedabudget.com/v1/budgets";
+    let url_base = "https://api.youneedabudget.com/v1/";
+
+    let url_budgets = [url_base, "budgets"].join("");
+
+    //     let url2 = "https://api.youneedabudget.com/v1/budgets/{budget_id}/transactions
+    // List transactions"
     let bearer_token = token::get_token("bearer.token"); // store token in private file
     let token_header = ["Authorization: Bearer", bearer_token.as_str()].join(" ");
     let mut list = List::new();
     list.append(&token_header).unwrap(); // Create a list of header items
     easy.get(true).unwrap(); // this is a get
     easy.http_headers(list).unwrap(); // Add the heder list to the http header
-    easy.url(url).unwrap(); // load the url
+    easy.url(&url_budgets).unwrap(); // load the url
     easy.perform().unwrap(); // Execute the http get
 
     assert_eq!(easy.response_code().unwrap(), 200); // Hope for a 200 rc
@@ -38,4 +43,23 @@ fn main() {
     println!("{:#}", parsed); // pretty print the json response
 
     println!("id: {}", parsed["data"]["budgets"][0]["id"]); // print the id
+
+    let id = &parsed["data"]["budgets"][0]["id"];
+
+    let url_transactions = [url_base, "budgets/", id.as_str().unwrap(), "/transactions"].join("");
+
+    println!("url_transactions: {}", url_transactions);
+
+    let mut easy2 = Easy2::new(Collector(Vec::new()));
+    easy2.url(&url_transactions).unwrap(); // load the url
+    let mut list2 = List::new();
+    list2.append(&token_header).unwrap(); // Create a list of header items
+
+    easy2.http_headers(list2).unwrap(); // Add the heder list to the http header
+
+    easy2.get(true).unwrap();
+    easy2.perform().unwrap(); // Execute the http get
+    let contents_json2 = easy2.get_ref(); // Get the data from the http response
+    let parsed_transactions2 = json::parse(&String::from_utf8_lossy(&contents_json2.0)).unwrap();
+    println!("{:#}", parsed_transactions2); // pretty print the json response
 }
